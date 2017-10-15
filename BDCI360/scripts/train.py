b160e5371a3ecddbec6c2694e520320fd46547c9
KillersDeath <liujiezhang@bupt.edu.cn>
2017-10-15 12:08:28 +0800

    add load_predict

--- BDCI360/scripts/train.py
+++ BDCI360/scripts/train.py
@@ -12,14 +12,15 @@
 import json
 import os
 import time
+from math import ceil
 
 import numpy as np
-from math import ceil
 import tensorflow as tf
 import yaml
 from keras import backend as K
 from utils.cnn_rnn import CNNRNN
 from utils.data_helper import load_data_cv
+
 K.set_learning_phase(1)
 
 
@@ -30,16 +31,20 @@ def do_eval(sess, model, eval_data, batch_size):
     K.set_learning_phase(0)
     number_of_data = len(eval_data)
     labels, probs = [], []
+    number_of_batch = ceil(number_of_data / batch_size)
     eval_loss, eval_cnt = 0., 0.
-    for start, end in zip(
-            range(0, number_of_data, batch_size),
-            range(batch_size, number_of_data, batch_size)):
+    for batch in range(number_of_batch):
+        start = batch_size * batch
+        end = start + min(batch_size, number_of_data - start)
+
         curr_titles = [article.deal_title for article in eval_data[start:end]]
-        curr_contents = [article.deal_content for article in eval_data[start:end]]
-        curr_labels = [
-            article.deal_judge for article in eval_data[start:end]
+        curr_contents = [
+            article.deal_content for article in eval_data[start:end]
         ]
-        curr_content_repeat = [article.content_repeat for article in eval_data[start:end]]
+        curr_labels = [article.deal_judge for article in eval_data[start:end]]
+        curr_content_repeat = [
+            article.content_repeat for article in eval_data[start:end]
+        ]
 
         curr_loss, curr_probs = sess.run(
             [model.loss, model.probs],
@@ -75,7 +80,11 @@ def train(params):
     '''
     train and eval.
     '''
-    datas, vocab = load_data_cv(file_path='../docs/data/train.tsv', voc_path='../docs/data/voc.json', mode='train', cv=10)
+    datas, vocab = load_data_cv(
+        file_path='../docs/data/train.tsv',
+        voc_path='../docs/data/voc.json',
+        mode='train',
+        cv=10)
 
     params['title_dim'] = len(datas[0].deal_title)
     params['content_dim'] = len(datas[0].deal_content)
@@ -107,7 +116,7 @@ def train(params):
     # add model saver, default save lastest 4 model checkpoints
     model_name = params['model_dir'] + params['model_name']
 
-    with tf.Session(config=config) as sess:
+    with tf.Session(config=config) as sess, tf.device('/gpu:1'):
         cnn_rnn = CNNRNN(params)
 
         saver = tf.train.Saver(max_to_keep=4)
@@ -127,10 +136,19 @@ def train(params):
                     range(0, number_of_training_data, batch_size),
                     range(batch_size, number_of_training_data, batch_size)):
                 step += 1
-                titles = [article.deal_title for article in train_datas[start:end]]
-                contents = [article.deal_content for article in train_datas[start:end]]
-                labels = [article.deal_judge for article in train_datas[start:end]]
-                repeat = [article.content_repeat for article in train_datas[start:end]]
+                titles = [
+                    article.deal_title for article in train_datas[start:end]
+                ]
+                contents = [
+                    article.deal_content for article in train_datas[start:end]
+                ]
+                labels = [
+                    article.deal_judge for article in train_datas[start:end]
+                ]
+                repeat = [
+                    article.content_repeat
+                    for article in train_datas[start:end]
+                ]
 
                 trn_loss, trn_probs, trn_acc, _ = sess.run(
                     [
@@ -181,20 +199,37 @@ def train(params):
                         step)
                     # judge whether test_acc is greater than before
                     if tst_acc >= best_acc:
-                        best_acc = tst_acc
                         best_step = step
                         best_sess = sess
-                        saver.save(best_sess, model_name + '-%s' % best_acc, global_step=step, write_meta_graph=False)
+                        saver.save(
+                            best_sess,
+                            model_name + '-%s' % tst_acc,
+                            global_step=step,
+                            write_meta_graph=not bool(best_acc))
+                        best_acc = tst_acc
                     # 早停止
                     if step - best_step > params['early_stop_eval_n'] * params['eval_test_batch']:
                         test_writer.close()
                         train_writer.close()
                         # predict and save train data
-                        predict(best_sess, cnn_rnn, datas, batch_size, save_name='train.csv')
+                        predict(
+                            best_sess,
+                            cnn_rnn,
+                            datas,
+                            batch_size,
+                            save_name='train.csv')
                         # predict and save eval data
-                        eval_public, vocab = load_data_cv(file_path='../docs/data/evaluation_public.tsv',
-                                                    voc_path='../docs/data/voc.json', mode='eval', cv=10)
-                        predict(best_sess, cnn_rnn, eval_public, batch_size, save_name='eval_public.csv')
+                        eval_public, vocab = load_data_cv(
+                            file_path='../docs/data/evaluation_public.tsv',
+                            voc_path='../docs/data/voc.json',
+                            mode='eval',
+                            cv=10)
+                        predict(
+                            best_sess,
+                            cnn_rnn,
+                            eval_public,
+                            batch_size,
+                            save_name='eval_public.csv')
                         exit()
 
         test_writer.close()
@@ -202,9 +237,17 @@ def train(params):
         # predict and save train data
         predict(best_sess, cnn_rnn, datas, batch_size, save_name='train.csv')
         # predict and save eval data
-        eval_public, vocab = load_data_cv(file_path='../docs/data/evaluation_public.tsv',
-                                          voc_path='../docs/data/voc.json', mode='eval', cv=10)
-        predict(best_sess, cnn_rnn, eval_public, batch_size, save_name='eval_public.csv')
+        eval_public, vocab = load_data_cv(
+            file_path='../docs/data/evaluation_public.tsv',
+            voc_path='../docs/data/voc.json',
+            mode='eval',
+            cv=10)
+        predict(
+            best_sess,
+            cnn_rnn,
+            eval_public,
+            batch_size,
+            save_name='eval_public.csv')
 
 
 def predict(sess, model, dataset, batch_size, save_name='eval.csv'):
@@ -214,7 +257,7 @@ def predict(sess, model, dataset, batch_size, save_name='eval.csv'):
     print('start to predict labels.....')
     K.set_learning_phase(0)
     number_of_data = len(dataset)
-    number_of_batch = ceil(number_of_data/batch_size)
+    number_of_batch = ceil(number_of_data / batch_size)
 
     with open('../docs/result/%s' % save_name, 'w') as f:
         for batch in range(number_of_batch):
@@ -222,10 +265,16 @@ def predict(sess, model, dataset, batch_size, save_name='eval.csv'):
             start = batch_size * batch
             end = start + min(batch_size, number_of_data - start)
 
-            curr_titles = [article.deal_title for article in dataset[start:end]]
-            curr_contents = [article.deal_content for article in dataset[start:end]]
+            curr_titles = [
+                article.deal_title for article in dataset[start:end]
+            ]
+            curr_contents = [
+                article.deal_content for article in dataset[start:end]
+            ]
 
-            curr_content_repeat = [article.content_repeat for article in dataset[start:end]]
+            curr_content_repeat = [
+                article.content_repeat for article in dataset[start:end]
+            ]
 
             curr_preds = sess.run(
                 model.preds,
@@ -238,17 +287,60 @@ def predict(sess, model, dataset, batch_size, save_name='eval.csv'):
 
             # transform [1] -> 'POSITIVE'
             for i in range(start, end):
-                dataset[i].predict = ['NEGATIVE', 'POSITIVE'][curr_preds[i - start]]
+                dataset[i].predict = ['POSITIVE',
+                                      'NEGATIVE'][curr_preds[i - start]]
                 line = '{}\t{}\t{}\t{}\t{}\n'.format(
-                    dataset[i].id,
-                    dataset[i].title,
-                    dataset[i].content,
-                    dataset[i].judge,
-                    dataset[i].predict
-                )
+                    dataset[i].id, dataset[i].title, dataset[i].content,
+                    dataset[i].judge, dataset[i].predict)
                 f.write(line)
 
     K.set_learning_phase(1)
+
+
+def load_predict(model_meta_path,
+                 predict_path,
+                 save_name='eval_public.csv',
+                 mode='eval',
+                 batch_size=128):
+    '''
+    load lastest model and predict datas.
+    :return:
+    '''
+    # 动态申请gpu，用多少申请多少
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+
+    with tf.Session(config=config) as sess:
+        saver = tf.train.import_meta_graph(model_meta_path)
+        saver.restore(sess, tf.train.latest_checkpoint('../docs/model/'))
+
+        # load graph
+        graph = tf.get_default_graph()
+
+        # get input placeholder
+        tf_title = graph.get_tensor_by_name('titles:0')
+        tf_content = graph.get_tensor_by_name('content:0')
+        tf_content_repeat = graph.get_tensor_by_name('content_repeat:0')
+
+        tf_preds = graph.get_tensor_by_name('predictions:0')
+
+        model = TFModel(tf_title, tf_content, tf_content_repeat, tf_preds)
+
+        # predict and save eval data
+        datas, vocab = load_data_cv(
+            file_path=predict_path,
+            voc_path='../docs/data/voc.json',
+            mode=mode,
+            cv=10)
+        predict(sess, model, datas, batch_size, save_name=save_name)
+
+
+class TFModel(object):
+    def __init__(self, title, content, content_repeat, preds):
+        self.titles = title
+        self.content = content
+        self.content_repeat = content_repeat
+        self.preds = preds
 
 
 if __name__ == '__main__':
@@ -256,3 +348,11 @@ if __name__ == '__main__':
     params = yaml.load(open('./utils/params.yaml', 'r'))
 
     train(params)
+
+    # 加载模型，进行数据预测
+    # load_predict(
+    #     model_meta_path='../docs/model/best.meta',
+    #     predict_path='../docs/data/evaluation_public.tsv',
+    #     save_name='eval_public.csv',
+    #     mode='eval',
+    #     batch_size=128)
