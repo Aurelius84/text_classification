7b86eaa162d7d9023106738d9042fcf2415a927e
moqi <185986777@qq.com>
2017-10-27 11:52:03 +0800

    去掉组合特征，添加词属性

--- BDCI360/scripts/utils/data_helper.py
+++ BDCI360/scripts/utils/data_helper.py
@@ -11,6 +11,7 @@ Competition url: http://www.datafountain.cn/#/competitions/276/intro
 
 import csv
 import json
+import jieba
 import os.path
 import re
 import pandas as pd
@@ -19,7 +20,7 @@ import numpy as np
 
 
 class ArticleSample(object):
-    def __init__(self, _id, title, deal_title, content, deal_content, judge,
+    def __init__(self, _id, title, deal_title, content, deal_content, word_content, judge,
                  deal_judge, cv):
         """
         文章对象
@@ -27,7 +28,8 @@ class ArticleSample(object):
         :param title:  文章标题(str)
         :param deal_title: 处理后的文章标题(list)
         :param content:  文章内容(str)
-        :param deal_content:  处理后的文章内容(list)
+        :param deal_content:  处理后的文章字表示(list)
+        :param word_content:  处理后的文章词表示(list)
         :param judge:  结果(str: NEGATIVE或POSITIVE)
         :param deal_judge: 结果(1表示POSITIVE，0表示NEGATIVE)
         :param cv: 交叉验证标记(int)
@@ -41,6 +43,8 @@ class ArticleSample(object):
         self.cv = cv
         self.deal_title = deal_title
         self.deal_content = deal_content
+        self.word_content = word_content
+
         self.deal_judge = deal_judge
         self.content_repeat = 0
         # 真实长度对1000做归一
@@ -64,72 +68,93 @@ class ArticleSample(object):
 
 
 
-def build_vocab(file_path, voc_path):
+def build_vocab(file_path, char_voc_path, word_voc_path):
     """
     建立词典
     :param file_path: 建立词典文件地址
-    :param voc_path: 词典保存地址
+    :param char_voc_path: 字的词典保存地址
+    :param word_voc_path: 词的词典保存地址
     :return:
     """
     print('build vocab...')
-    voc = {'<s>': 0}
-    voc_index = 0
-    max_title_length = 0
-    max_content_length = 0
+    char_voc,word_voc = {'<s>': 0},{'<s>':0}
+    char_voc_index,word_voc_index = 0,0
+    char_max_title_length,word_max_title_length = 0,0
+    char_max_content_length,word_max_content_length = 0,0
 
     train = pd.read_table(file_path, sep='\\t', encoding='utf-8', header=None,
                           engine='python')
 
     for title, content in zip(train[1],train[2]):
         title,content = str(title),str(content)
-        max_title_length = max(max_title_length, len(title))
-        max_content_length = max(max_content_length, len(content))
-        # if len(content) == 93749:
-        #     print(title)
-        #     print(content)
-        for x in title:
-            if x not in voc:
-                voc_index += 1
-                voc[x] = voc_index
-        for x in content:
-            if x not in voc:
-                voc_index += 1
-                voc[x] = voc_index
+        char_max_title_length = max(char_max_title_length, len(title))
+        char_max_content_length = max(char_max_content_length, len(content))
+        for x in title+content:
+            if x not in char_voc:
+                char_voc_index += 1
+                char_voc[x] = char_voc_index
+        word_title = list(filter(lambda x:x!=' ',[w for w in jieba.cut(str(title).lower().strip())]))
+        word_content = list(filter(lambda x: x != ' ', [w for w in jieba.cut(str(content).lower().strip())]))
+        word_max_title_length = max(word_max_title_length,len(word_title))
+        word_max_content_length = max(word_max_content_length, len(word_content))
+        word_content.extend(word_title)
+        for x in word_content:
+            if x not in word_voc:
+                word_voc_index +=1
+                word_voc[x] = word_voc_index
 
     print('build vocab done')
-    voc_dict = {
-        'voc': voc,
-        'max_title_length': max_title_length,
-        'max_content_length': max_content_length
+    char_voc_dict = {
+        'voc': char_voc,
+        'max_title_length': char_max_title_length,
+        'max_content_length': char_max_content_length
     }
-    with open(voc_path, 'w') as f:
-        json.dump(voc_dict, f)
-    return [voc, max_title_length, max_content_length]
+    word_voc_dict = {
+        'voc': word_voc,
+        'max_title_length': word_max_title_length,
+        'max_content_length': word_max_content_length
+    }
+    with open(char_voc_path, 'w') as f:
+        json.dump(char_voc_dict, f)
+    with open(word_voc_path, 'w') as f:
+        json.dump(word_voc_dict, f)
+    return [char_voc, char_max_title_length, char_max_content_length,word_voc, word_max_title_length,word_max_content_length]
 
 
-def load_data_cv(file_path, voc_path, mode, cv=5):
+def load_data_cv(file_path, char_voc_path,word_voc_path, mode, cv=5):
     """
     加载训练数据、测试数据
     :param file_path:  数据地址
-    :param voc_path:  词典地址（建立词典之后直接可以加载）
+    :param char_voc_path:  字的词典地址（建立词典之后直接可以加载）
+    :param word_voc_path:  词的词典地址（建立词典之后直接可以加载）
     :param mode:  是训练数据(train)还是测试数据(test)
     :param cv:  几折交叉验证
     :return:
     """
     rev = []
-    if os.path.isfile(voc_path):
-        with open(voc_path, 'r') as f:
+    if os.path.isfile(char_voc_path):
+        with open(char_voc_path, 'r') as f:
             voc_dict = json.load(f)
-            voc = voc_dict['voc']
-            max_title_length = voc_dict['max_title_length']
-            max_content_length = voc_dict['max_content_length']
+            char_voc = voc_dict['voc']
+            char_max_title_length = voc_dict['max_title_length']
+            char_max_content_length = voc_dict['max_content_length']
+        with open(word_voc_path, 'r') as f:
+            voc_dict = json.load(f)
+            word_voc = voc_dict['voc']
+            word_max_title_length = voc_dict['max_title_length']
+            word_max_content_length = voc_dict['max_content_length']
     else:
-        voc, max_title_length, max_content_length = build_vocab(
-            file_path, voc_path)
-    max_content_length = min(max_content_length, 1000)
-    print('len voc: ', len(voc))
-    print('max_title_length: ', max_title_length)
-    print('max_content_length: ', max_content_length)
+        char_voc, char_max_title_length, char_max_content_length,\
+        word_voc, word_max_title_length, word_max_content_length\
+            = build_vocab(file_path, char_voc_path, word_voc_path)
+    char_max_content_length = min(char_max_content_length, 1000)
+    word_max_content_length = min(word_max_content_length, 1000)
+    print('len char voc: ', len(char_voc))
+    print('char_max_title_length: ', char_max_title_length)
+    print('char_max_content_length: ', char_max_content_length)
+    print('len word voc: ', len(word_voc))
+    print('word_max_title_length: ', word_max_title_length)
+    print('word_max_content_length: ', word_max_content_length)
 
     df = pd.read_table(file_path, sep='\\t', encoding='utf-8', header=None,
                           engine='python')
@@ -143,13 +168,15 @@ def load_data_cv(file_path, voc_path, mode, cv=5):
         if cnt % 20000 == 0:
             print('load data:...', cnt)
 
-        pad_title, pad_content = title[:
-                                       max_title_length], content[:
-                                                                  max_content_length]
-        deal_title = [voc[x] if x in voc else 0 for x in pad_title]
-        deal_title.extend([0] * (max_title_length - len(deal_title)))
-        deal_content = [voc[x] if x in voc else 0 for x in pad_content]
-        deal_content.extend([0] * (max_content_length - len(deal_content)))
+        content_word = list(filter(lambda x: x != ' ', [w for w in jieba.cut(str(content).lower().strip())]))
+        pad_title, pad_content = title[:char_max_title_length], content[:char_max_content_length]
+        pad_content_word = content_word[:word_max_content_length]
+        deal_title = [char_voc[x] if x in char_voc else 0 for x in pad_title]
+        deal_title.extend([0] * (char_max_title_length - len(deal_title)))
+        deal_content = [char_voc[x] if x in char_voc else 0 for x in pad_content]
+        deal_content.extend([0] * (char_max_content_length - len(deal_content)))
+        word_content = [word_voc[x] if x in word_voc else 0 for x in pad_content_word]
+        word_content.extend([0] * (word_max_content_length-len(pad_content_word)))
         deal_judge = [1, 0] if judge == 'POSITIVE' else [0, 1]
         article = ArticleSample(
             _id=_id,
@@ -157,6 +184,7 @@ def load_data_cv(file_path, voc_path, mode, cv=5):
             deal_title=deal_title,
             content=content,
             deal_content=deal_content,
+            word_content=word_content,
             judge=judge,
             deal_judge=deal_judge,
             cv=np.random.randint(0, cv))
@@ -169,6 +197,7 @@ def load_data_cv(file_path, voc_path, mode, cv=5):
     # print(rev[0].deal_title)
     # print(rev[0].content)
     # print(rev[0].deal_content)
+    # print(rev[0].word_content)
     # print(rev[0].judge)
     # print(rev[0].deal_judge)
     cnt, all = 0, 0
@@ -179,12 +208,13 @@ def load_data_cv(file_path, voc_path, mode, cv=5):
             cnt += 1
         all += 1.0
     print(cnt, all, cnt / all)
-    return rev, voc
+    return rev, char_voc, word_voc
 
 
 if __name__ == '__main__':
-    file_path_train = '../../docs/data/train.tsv'
-    file_path_test = '../../docs/data/evaluation_public.tsv'
-    voc_path = '../../docs/data/voc.json'
-    load_data_cv(file_path_train, voc_path, 'train')
-    load_data_cv(file_path_test, voc_path, 'eval')
+    file_path_train = '../../docs/data/train_1000.tsv'
+    file_path_test = '../../docs/data/evaluation_public_1000.tsv'
+    char_voc_path = '../../docs/data/char_voc.json'
+    word_voc_path = '../../docs/data/word_voc.json'
+    load_data_cv(file_path_train, char_voc_path,word_voc_path, 'train')
+    # load_data_cv(file_path_test, voc_path, 'eval')
