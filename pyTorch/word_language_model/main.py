38c48fa8fb83ee3c8b4ef2600078a6ba916bf24f
KillersDeath <KillersDeath@users.noreply.github.com>
2017-11-14 10:35:38 +0800

    ms -> s

--- pyTorch/word_language_model/main.py
+++ pyTorch/word_language_model/main.py
@@ -150,7 +150,7 @@ def train():
         if batch % args.log_interval == 0 and batch > 0:
             cur_loss = total_loss[0] / args.log_interval
             elapsed = time.time() - start_time
-            print('| epoch {:3d} | {:5d}/{:5d} batches | lr {:02.2f} | ms/batch {:5.2f} | '
+            print('| epoch {:3d} | {:5d}/{:5d} batches | lr {:02.2f} | s/batch {:5.2f} | '
                   'loss {:5.2f} | ppl {:8.2f}'.format(
                 epoch, batch, len(train_data) // args.bptt, lr,
                               elapsed * 1000 / args.log_interval, cur_loss, math.exp(cur_loss)))
